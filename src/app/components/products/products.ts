28c03a1cf362e87530f1932bd82a0a44a4ad8113
jesus <galvancillo1994@gmail.com>
2025-07-12 15:15:53 -0500

    implementacion dialog

--- src/app/components/products/products.ts
+++ src/app/components/products/products.ts
@@ -1,14 +1,24 @@
 import { Component, Input, } from '@angular/core';
 import { SlicePipe, CommonModule } from '@angular/common';
 import { SkeletonModule } from 'primeng/skeleton';
+import { DialogModule } from 'primeng/dialog';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-products',
-  imports: [CommonModule, SlicePipe, SkeletonModule  ],
+  imports: [CommonModule, SlicePipe, SkeletonModule, DialogModule, ButtonModule ],
   templateUrl: './products.html',
   styleUrl: './products.scss'
 })
 export class Products {
 
   @Input() products: any[] = [];
+  visible = false
+  productSelect: any = {};
+
+  showDialog(product: any) {
+    this.visible = true
+    this.productSelect = product
+  }
+
 }
